7ca4e4319b7a28164a3376d990d7202af0da0b92
Mahmoud Selmy <mahmoudselmy06@gmail.com>
2020-06-02 19:17:14 +0200

    + Task3: define message queue

--- src/TrafficLight.h
+++ src/TrafficLight.h
@@ -12,16 +12,19 @@ class Vehicle;
 
 // FP.3 Define a class „MessageQueue“ which has the public methods send and receive. 
 // Send should take an rvalue reference of type TrafficLightPhase whereas receive should return this type. 
-// Also, the class should define an std::dequeue called _queue, which stores objects of type TrafficLightPhase. 
+// Also, the class should define an std::dequeue called {_queue}, which stores objects of type TrafficLightPhase. 
 // Also, there should be an std::condition_variable as well as an std::mutex as private members. 
 
 template <class T>
 class MessageQueue
 {
 public:
-
+    T receive();
+    void send(T &&msg);
 private:
-    
+    std::mutex _mutex;
+    std::condition_variable _condtidion;
+    std::deque<T> _queue;
 };
 
 // FP.1 : Define a class „TrafficLight“ which is a child class of TrafficObject. 
